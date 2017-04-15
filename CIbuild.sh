b8a008b0fd59feb346686df9afde348b0e569531
Bond-009 <bond.009@outlook.com>
2017-04-15 17:55:31 +0200

    Fix tests

--- CIbuild.sh
+++ CIbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-dotnet restore && dotnet build $TRAVIS_SOLUTION -c Release
+dotnet restore && dotnet build $TRAVIS_SOLUTION
 cd test/OpenWeather.Net.Tests
-dotnet restore && dotnet xunit -c Release
+dotnet restore && dotnet xunit
